9b7b7bcdb620babb2c21244087cf38010e27b237
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-06-06 16:46:33 +0200

    Added display msg when dividing by 0

--- script.js
+++ script.js
@@ -3,54 +3,58 @@ let operator = '';
 let secondNumber = '';
 
 function appendNumber(number) {
-  if (operator === '') {
-    firstNumber += number;
-    updateDisplay(firstNumber);
-  } else {
-    secondNumber += number;
-    updateDisplay(secondNumber);
-  }
+    if (operator === '') {
+        firstNumber += number;
+        updateDisplay(firstNumber);
+    } else {
+        secondNumber += number;
+        updateDisplay(secondNumber);
+    }
 }
 
 function setOperator(op) {
-  operator = op;
+    operator = op;
 }
 
 function clearDisplay() {
-  firstNumber = '';
-  operator = '';
-  secondNumber = '';
-  updateDisplay('0');
+    firstNumber = '';
+    operator = '';
+    secondNumber = '';
+    updateDisplay('0');
 }
 
 function calculate() {
-  let result;
-  const num1 = parseFloat(firstNumber);
-  const num2 = parseFloat(secondNumber);
+    let result;
+    const num1 = parseFloat(firstNumber);
+    const num2 = parseFloat(secondNumber);
 
-  switch (operator) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      result = num1 / num2;
-      break;
-    default:
-      return;
-  }
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                updateDisplay("You have created a black hole!");
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            return;
+    }
 
-  updateDisplay(result);
-  firstNumber = result.toString();
-  operator = '';
-  secondNumber = '';
+    updateDisplay(result);
+    firstNumber = result.toString();
+    operator = '';
+    secondNumber = '';
 }
 
 function updateDisplay(value) {
-  document.getElementById('display').value = value;
+    document.getElementById('display').value = value;
 }
